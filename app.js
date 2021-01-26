8f33c59a9cc0dc3b65051358ec3c439930cabd55
naruthsithasom <ett0088@gmail.com>
2021-01-27 01:20:59 +0700

    success

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ const source = {
 const pool = mysql.createPool(source)
 const readBody = express.urlencoded({ extended: false })
 
-let cookieValid = []
-let model = {}
+let cookieValid = [ ]
+let model = { }
+//let resource = [ ]
 
-let resource = [ ]
 
 app.listen(5000, showStatus)
 app.engine('html', ejs.renderFile)
@@ -43,48 +43,36 @@ app.get('/profile', readCookie, showProfile)
 
 app.get('/logout', readCookie, gotoLogout)
 
-app.get('/list', testProfile)
-
 app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
 app.use(express.static('photo'))
 app.use(express.static('reports'))
 app.use((req, res) => { res.status(404).render('error.html') })
-
-function testProfile(req, res){
-    let t = { }
-
-    t.report = resource
-    console.log(t.report)
-    //let Obj =  JSON.stringify(t.report)
-    //fs.writeFileSync('reports/rp01.json', Obj)
-    //res.redirect('/login')
-}
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
 
 function showStatus() {
     let date = new Date()
     console.log(`>: Server is started. ${date} `)
 }
+// --------------------------------------------------------------------------
 
- function showDashboard(req, res) {
+ async function showDashboard(req, res) {
     let card = null
     if (req.cookies != null) {
         card = req.cookies.card
     }
     if (cookieValid[card]) {
-         pool.query('select * from staff', (error, result) => {
-            model.stafflists = result
-        })
-         pool.query('select * from report', (error, result) => {
-            model.report = result
-            //res.status(202)
+         await pool.query('select id, name, data, date_times  from staff  right join report on staff.id = report.staff_id', (error, result) => {
+            model.dashboard = result
             res.render('dashboard.html', model)
         })
     } else {
         res.redirect('/login')
     }
 }
+// --------------------------------------------------------------------------
 
 function checkList(req, res) {
     let card = null
@@ -97,52 +85,88 @@ function checkList(req, res) {
         res.redirect('/login')
     }
 }
+// --------------------------------------------------------------------------
 
 function saveNewList(req, res) {
 
     let sql = 'INSERT INTO REPORT (data, staff_id) values (?, ?)'
-    let data_ = ""
-    let result = [
-        req.body.inputGroup1, req.body.btnRadio1, req.body.btnRadio2,
-        req.body.selected1, req.body.selected2, req.body.selected3,
-        req.body.selected4, req.body.selected5, req.body.selected6,
-        req.body.btnRadio3, req.body.btnRadio4, req.body.btnRadio5,
-        req.body.btnRadio6, req.body.btnRadio7, req.body.btnRadio8,
-        req.body.btnRadio9, req.body.btnRadio10, req.body.btnRadio11,
-        req.body.btnRadio12, req.body.btnRadio13, req.body.btnRadio14,
-        req.body.btnRadio15, req.body.btnRadio16, req.body.btnRadio17,
-        req.body.btnRadio18, req.body.btnRadio19, req.body.btnRadio20,
-        req.body.btnRadio21, req.body.btnRadio22, req.body.btnRadio23,
-        req.body.btnRadio24, req.body.btnRadio25, req.body.btnRadio26,
-        req.body.btnRadio27, req.body.btnRadio28, req.body.inputGroup2,
-    ]
-    let r = {}
-    resource = [...result]
-    r.list = resource
-    const save =  JSON.stringify(r)
+    //let data_ = ""
+    let result = {
+        'code1':req.body.inputGroup1,
+        'code2':req.body.btnRadio1, 
+        'code3':req.body.btnRadio2,
+        'code4':req.body.selected1,
+        'code5':req.body.selected2,
+        'code6': req.body.selected3,
+        'code7': req.body.selected4,
+        'code8': req.body.selected5,
+        'code9': req.body.selected6,
+        'code10': req.body.btnRadio3,
+        'code11': req.body.btnRadio4,
+        'code12': req.body.btnRadio5,
+        'code13': req.body.btnRadio6,
+        'code14': req.body.btnRadio7,
+        'code15': req.body.btnRadio8,
+        'code16': req.body.btnRadio9,
+        'code17': req.body.btnRadio10,
+        'code18': req.body.btnRadio11,
+        'code19': req.body.btnRadio12,
+        'code20': req.body.btnRadio13,
+        'code21': req.body.btnRadio14,
+        'code22': req.body.btnRadio15,
+        'code23': req.body.btnRadio16,
+        'code24': req.body.btnRadio17,
+        'code25': req.body.btnRadio18,
+        'code26': req.body.btnRadio19,
+        'code27': req.body.btnRadio20,
+        'code28': req.body.btnRadio21,
+        'code29': req.body.btnRadio22,
+        'code30': req.body.btnRadio23,
+        'code31': req.body.btnRadio24,
+        'code32': req.body.btnRadio25,
+        'code33': req.body.btnRadio26,
+        'code34': req.body.btnRadio27,
+        'code35': req.body.btnRadio28,
+        'code35': req.body.inputGroup2
+    } 
+    let resource = JSON.stringify(result).replace(/[\/\(\)\']/g, "&apos;");
 
-    const tvb = model.user.map( x => x.tvb)
+    //resource = [...result] //Global Store
     
-    let dmy = ''
-    let t = new Date();
-    let time = t.toDateString()
+    // let source = [...result]
+    // let levy = [ ] 
+    // for(let key in source) {
+        
+    //     let code = `code${key}:`+source[key]
+    //     levy.push(code)
+    // }
+    // //model.levy = levy //Global Store
+    // let obj = { }
+    // obj.list = levy
+    // const save =  JSON.stringify(obj)
 
-    for( i=0; i<time.length; i++ ) {
-        if(time.charAt(i) === ' ') continue;
-            dmy += time.charAt(i)
-        }  
-    const dir = `/reports/${tvb}-${dmy}.json`
+    // const tvb = model.user.map( x => x.tvb)
     
-    fs.writeFileSync('reports/'+ tvb + dmy.toLocaleLowerCase() +'.json', save)
+    // let dmy = ''
+    // let t = new Date();
+    // let time = t.toDateString()
+
+    // for( i=0; i<time.length; i++ ) {
+    //     if(time.charAt(i) === ' ') continue;
+    //         dmy += time.charAt(i)
+    //     }  
+    // const dir = `/reports/${tvb}-${dmy}.json`
     
-    for (let i in result) { 
-        data_ = data_ + '-' + result[i] 
-    }
+    // fs.writeFileSync('reports/'+ tvb + dmy.toLocaleLowerCase() +'.json', save)
+    
+    // for (let i in result) { data_ = data_ + '-' + result[i] }
 
     //let m = model.user
-    let staff_ = model.user.find(x => x.id)
-    let data = [data_, Number(staff_.id)]
+    let staff_ = model.user.find(x => x.id) //Global 
+    // let data = [obj.list.toString(), Number(staff_.id)]
+     let data = [resource, Number(staff_.id)]
 
+    //pool.query(sql, data, (err) => {
     pool.query(sql, data, (err) => {
         if (err == null) {
             res.render('dashboard.html', model)
@@ -150,14 +174,15 @@ function saveNewList(req, res) {
         } else { console.log('Oh,It is not to a insert to database: ', err) }
     })
 }
+// --------------------------------------------------------------------------
 
 function showRegister(req, res) {
     res.render('register.html')
 }
-
+// --------------------------------------------------------------------------
 async function saveRegister(req, res) {
 
-    let sql = 'insert into staff (tvb,name,position,password) values (?,?,?,sha2(?,512))'
+    let sql = 'insert into staff (tvb, name, position, password) values (?, ?, ?, sha2(?, 512))'
     let data = [req.body.tvb, req.body.name, req.body.position, req.body.password]
 
     await pool.query(sql, data, (error) => {
@@ -169,34 +194,13 @@ async function saveRegister(req, res) {
         res.render('login.html')
     })
 }
+// --------------------------------------------------------------------------
 
 function showLogin(req, res) {
     res.render('login.html')
 }
+// --------------------------------------------------------------------------
 
-// async function checkLogin(req, res) {
-
-//     let sql = `SELECT * FROM staff WHERE tvb=? AND password=sha2(?,512)`
-//     let data = [req.body.tvb, req.body.password]
-
-//     await pool.query(sql, data, (err, result) => {
-//         model.user = result
-//         if (result.length == 1) {
-//             let card = generateKey()
-//             cookieValid[card] = result[0]
-//             res.status(202)
-//             res.header('Set-Cookie', 'card=' + card)
-//             res.render('dashboard.html', model)
-//         }
-//         else {
-//             console.error(err)
-//             res.redirect('/login')
-//         }
-//     })
-//      pool.query('select * from report', (error, result) => {
-//         model.report = result
-//     })
-// }
 async function checkLogin(req, res) {
 
     let sql = `SELECT * FROM staff WHERE tvb=? AND password=sha2(?,512)`
@@ -210,8 +214,8 @@ async function checkLogin(req, res) {
             res.status(202)
             res.header('Set-Cookie', 'card=' + card)
         } if(result.length == 1){
-            pool.query('select * from report', (error, result) => {
-                model.report = result
+            pool.query('select id, name, data, date_times  from staff  right join report on staff.id = report.staff_id', (error, result) => {
+                model.dashboard = result
                 res.render('dashboard.html', model)
             })
         }
@@ -233,12 +237,14 @@ function generateKey() {
     }
     return c.join('-')
 }
+// --------------------------------------------------------------------------
 
 function gotoLogout(req, res) {
     let card = req.cookies.card
     cookieValid[card] = null
     res.redirect('/login')
 }
+// --------------------------------------------------------------------------
 
 function showProfile(req, res) {
     let card = null
@@ -256,7 +262,13 @@ function showProfile(req, res) {
             if(i) {
                 pool.query('select * from report where staff_id = ?',[i], (err, result) => {
                     model.profile = result
-                    res.render('profile.html', model)
+        
+                    // let t = model.profile.map( x => x.date_times) 
+                    // let filter = JSON.stringify(t)
+                    // let time = filter.slice(2, 20) //"2021-01-26T08:44:5"
+                    // model.local = time
+                    res.render('profile.html', model) 
+                    //res.send(time.slice(2,20)) //2021-01-26T08:44:5
                     //res.send(model)
                 })
             }else{
@@ -268,6 +280,7 @@ function showProfile(req, res) {
         res.redirect('/login')
     }
 }
+// --------------------------------------------------------------------------
 
 /*
         <% for (let i in all) { %>
